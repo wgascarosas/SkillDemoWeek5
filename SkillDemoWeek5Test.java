8ed9ace05fbaad8885b06bd5e00a66e96fab8b6a
wgascarosas <97696374+wgascarosas@users.noreply.github.com>
2022-02-04 10:17:01 -0800

    changes

---
+++ SkillDemoWeek5Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoWeek5Test {
+    @Test
+    public void sub() {
+        assertEquals(25, SkillDemoWeek5.sub(50, 25));
+    }  
+}
